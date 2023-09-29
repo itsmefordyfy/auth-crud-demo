cc42dc1b7634e9c7903bbb938906ed5f58562fe4
Clifford Francisco <clifford.francisco@tracktile.io>
2023-09-29 19:35:55 +0800

    fix: data retrieval in home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,36 @@
-import { Text } from "react-native-paper";
+import { ActivityIndicator, Card, Text } from "react-native-paper";
+import { supabaseClient } from "../supabase-client";
+import { PromiseView } from "../components";
+import { UserInfo } from "../auth";
 
-export function HomeScreen() {
-  return <Text>This is home</Text>;
+type HomeProps = {
+  jwt: string;
+};
+
+type DisplayUser = Omit<UserInfo, "password" | "email">;
+
+export function HomeScreen({ jwt }: HomeProps) {
+  const fetchUser = async () => {
+    return (await supabaseClient.auth.getUser(jwt)).data.user;
+  };
+
+  return (
+    <PromiseView
+      promise={fetchUser()}
+      loadedView={(user) => {
+        if (user === null) return <ActivityIndicator />;
+
+        const metadata = user.user_metadata as DisplayUser;
+
+        return (
+          <Card>
+            <Text>{`Email: ${user.email}`}</Text>
+            <Text>{`First Name: ${metadata.firstName}`}</Text>
+            <Text>{`Last Name: ${metadata.lastName}`}</Text>
+            <Text>{`Phone: ${metadata.phoneNumber}`}</Text>
+          </Card>
+        );
+      }}
+    ></PromiseView>
+  );
 }
