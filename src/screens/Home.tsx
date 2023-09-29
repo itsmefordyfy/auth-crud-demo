c423801da8c9b98e52524c5752cf71c5de9f0047
Clifford Francisco <clifford.francisco@tracktile.io>
2023-09-29 21:47:49 +0800

    feat: add post

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,18 @@
-import { ActivityIndicator, Button, Card, Text } from "react-native-paper";
+import {
+  ActivityIndicator,
+  Button,
+  Card,
+  IconButton,
+  Text,
+  TextInput,
+} from "react-native-paper";
 import { supabaseClient } from "../supabase-client";
 import { PromiseView } from "../components";
 import { UserInfo, logoutUser } from "../auth";
 import { User } from "@supabase/supabase-js";
 import { router } from "expo-router";
-import { View } from "react-native";
+import { FlatList, View, TextInput as TextInputRef } from "react-native";
+import { createRef, useState } from "react";
 
 type HomeProps = {
   jwt: string;
@@ -71,6 +79,103 @@ function Content({ user }: ContentProps) {
           Logout
         </Button>
       </Card>
+      <PostViewer user={user} />
     </View>
+  );
+}
+
+function PostViewer({ user }: ContentProps) {
+  const getPosts = async () => {
+    return (await supabaseClient.from("Post").select()).data as {
+      id: string;
+      text: string;
+    }[];
+  };
+
+  const [refreshFlag, setRefreshFlag] = useState(false);
+  const [text, setText] = useState("");
+  const textRef = createRef<TextInputRef>();
+
+  const addPost = async () => {
+    await supabaseClient.from("Post").insert({
+      author: user.id,
+      text: text,
+    });
+
+    textRef.current?.clear();
+  };
+
+  const removePost = async (id: string) => {
+    await supabaseClient.from("Post").delete().eq("id", id);
+  };
+
+  return (
+    <PromiseView
+      promise={getPosts()}
+      refreshFlag={refreshFlag}
+      loadingView={<ActivityIndicator />}
+      loadedView={(data) => (
+        <FlatList
+          style={{
+            margin: 24,
+          }}
+          data={data}
+          renderItem={({ item }) => {
+            return (
+              <Card
+                key={item.id}
+                style={{
+                  alignSelf: "center",
+                  width: 300,
+                  height: 96,
+                  padding: 16,
+                  margin: 16,
+                }}
+              >
+                <Text>{item.text}</Text>
+                <IconButton
+                  icon="trash-can"
+                  mode="contained"
+                  size={20}
+                  onPress={() => {
+                    removePost(item.id).then(() => {
+                      setRefreshFlag(!refreshFlag);
+                    });
+                  }}
+                />
+              </Card>
+            );
+          }}
+          ListFooterComponent={
+            <Card
+              style={{
+                alignSelf: "center",
+                width: 300,
+                height: 150,
+                padding: 16,
+              }}
+            >
+              <TextInput
+                ref={textRef}
+                mode="outlined"
+                multiline
+                style={{ flex: 1 }}
+                onChangeText={(inputText) => setText(inputText)}
+              />
+              <IconButton
+                icon="shape-rectangle-plus"
+                mode="contained"
+                size={20}
+                onPress={() => {
+                  addPost().then(() => {
+                    setRefreshFlag(!refreshFlag);
+                  });
+                }}
+              />
+            </Card>
+          }
+        />
+      )}
+    />
   );
 }
