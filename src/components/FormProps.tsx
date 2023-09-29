0c47475c851f944f1157ace0ed744467fa56a317
Clifford Francisco <clifford.francisco@tracktile.io>
2023-09-29 15:19:07 +0800

    feat: implement form state with type validation

---
+++ src/components/FormProps.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+export type FormRecord = Record<string, string>;
+
+export type FormProps<T extends FormRecord> = {
+  formState: FormState<T>;
+  fieldName: keyof T;
+};
+
+export interface FormState<T extends FormRecord> {
+  state: T;
+  setState: (value: T) => void;
+}
+
+export function createFormState<T extends FormRecord>(
+  defaultValues: T
+): FormState<T> {
+  const [state, setState] = useState(defaultValues);
+
+  return { state, setState };
+}
